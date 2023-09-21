8d2160bc59268b799faa075802d03c9185e58d47
claudiodantas <claudiodantas1996@gmail.com>
2023-09-21 10:10:51 -0300

    remove the function passes as prop

--- meals_app_with_app_wide_state/lib/screens/meal_details.dart
+++ meals_app_with_app_wide_state/lib/screens/meal_details.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app_with_app_wide_state/models/meal.dart';
+import 'package:meals_app_with_app_wide_state/providers/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen(
-      {super.key, required this.meal, required this.onToggleFavorite});
+class MealDetailsScreen extends ConsumerWidget {
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
 
   final Meal meal;
-  final void Function(Meal meal) onToggleFavorite;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
-              onPressed: () => onToggleFavorite(meal),
+              onPressed: () {
+                final wasAdded = ref
+                    .read(favoriteMealsProvider.notifier)
+                    .toggleMealsFavoriteStatus(meal);
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(wasAdded
+                        ? 'Added as favorite'
+                        : 'Removed from favorite')));
+              },
               icon: const Icon(Icons.star))
         ],
       ),
